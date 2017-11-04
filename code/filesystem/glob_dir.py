a2337d259dc7b1ecfbcb315b3d25d1fc4826be22
Aleksej Kamynin <zendzirou@gmail.com>
2017-11-04 21:38:50 +0300

    more stubs

--- code/filesystem/glob_dir.py
+++ code/filesystem/glob_dir.py
@@ -2,3 +2,11 @@
 """
 Получить список файлов в каталоге по glob выражению
 """
+
+import glob
+
+print(glob.glob('_stub/*.txt'))
+print(glob.glob('_stub/**/*.txt', recursive=True))
+print(glob.glob('_stub/*'))
+
+
