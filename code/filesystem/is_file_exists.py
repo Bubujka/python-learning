8b54177956293872d5c29d721e5e22e566a07315
Aleksej Kamynin <zendzirou@gmail.com>
2017-11-04 21:29:08 +0300

    moar...

--- code/filesystem/is_file_exists.py
+++ code/filesystem/is_file_exists.py
@@ -2,3 +2,9 @@
 """
 Проверить что файл существует
 """
+import os
+
+print(os.path.exists('_stub/dirs.txt'))
+print(os.path.exists('_stub/dirs2.txt'))
+
+
--- code/filesystem/resolve_path.py
+++ code/filesystem/resolve_path.py
@@ -2,3 +2,7 @@
 """
 Раскрыть относительный путь в абсолютный
 """
+from os import path
+print(path.abspath('../bin/'))
+print(path.abspath('/home/bubujka/../zuzujka/.'))
+
