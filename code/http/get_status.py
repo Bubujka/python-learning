0b6498ddb6a130fd61d3a57726354d828ae59dd9
Aleksej Kamynin <zendzirou@gmail.com>
2017-11-13 12:47:57 +0300

    moar code

--- code/http/get_status.py
+++ code/http/get_status.py
@@ -2,3 +2,12 @@
 """
 После HTTP запроса - проверить status
 """
+
+import urllib3
+
+http = urllib3.PoolManager()
+r = http.request('POST', 'http://httpbin.org/post')
+
+print(r.status)
+print(r.data)
+
